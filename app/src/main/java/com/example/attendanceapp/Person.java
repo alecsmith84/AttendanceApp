21d09a1f77beb46fec72145b86fcb73811956b22
Uncle Troy <tda15a@acu.edu>
2018-10-29 14:20:08 -0500

    Added person class and rough people attendance. Also date stuff.

---
+++ app/src/main/java/com/example/attendanceapp/Person.java
@@ -0,0 +1,11 @@
+package com.example.attendanceapp;
+
+public class Person {
+
+    public Person(String NAME){
+        isHere = false;
+        name = NAME;
+    }
+    public boolean isHere;
+    public String name;
+}

fd9ddd495c1ba3d4159bd95639a13d12c6f34780
alecsmith84 <astonmartindbrs9>
2018-11-05 15:01:08 -0600

    take 2

---
+++ app/src/main/java/com/example/attendanceapp/Lessons.java
@@ -0,0 +1,20 @@
+package com.example.attendanceapp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class Lessons extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lessons);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+}

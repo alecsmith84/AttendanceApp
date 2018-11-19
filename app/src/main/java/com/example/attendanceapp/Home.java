0c6516134d43a9f0cb529030b67202e972d38f37
alecsmith84 <astonmartindbrs9>
2018-11-19 15:13:37 -0600

    deleted some comments

--- app/src/main/java/com/example/attendanceapp/Home.java
+++ app/src/main/java/com/example/attendanceapp/Home.java
@@ -19,21 +19,12 @@ public class Home extends AppCompatActivity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        //this is a test
-        //TROY WAS HERE
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       /* FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(

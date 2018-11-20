ea31f8bff03bf9e477d9facbee146e6259542537
alecsmith84 <astonmartindbrs9>
2018-11-19 22:33:32 -0600

    Alec is tired and doesn't remember what he changed in this version

--- app/src/main/java/com/example/attendanceapp/Home.java
+++ app/src/main/java/com/example/attendanceapp/Home.java
@@ -46,12 +46,6 @@ public class Home extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.home, menu);
-        return true;
-    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
